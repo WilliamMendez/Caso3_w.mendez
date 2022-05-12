b98a13b2c9020b19c3a817f61e0ff667bc00217e
WilliamMendez <69637352+WilliamMendez@users.noreply.github.com>
2022-05-11 21:37:18 -0500

    clientes opcionales

--- App/src/cliente/ClienteDelegado.java
+++ App/src/cliente/ClienteDelegado.java
@@ -4,6 +4,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Random;
+import java.util.Scanner;
 
 public class ClienteDelegado extends Cliente {
 
@@ -13,12 +14,15 @@ public class ClienteDelegado extends Cliente {
 
     public static void main(String[] args) throws Exception {
         String[] nombres = {"Juliana", "Camila", "Daniel", "Boris", "Juan", "Sergio", "Cristian", "Pedro", "Jesus", "William", "Kevin"};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de delegados: ");
+        int nDelegados = sc.nextInt();
         try {
             PublicKey llave = getPublicKey("keys", "RSA");
 
             // AppCliente clientes[] = new AppCliente[1];
             ClienteDelegado[] clientes = new ClienteDelegado[32];
-            for (int i = 0; i < 32; i++) {
+            for (int i = 0; i < nDelegados; i++) {
                 String nombreAleatorio = nombres[new Random().nextInt(nombres.length)];
                 clientes[i] = new ClienteDelegado(nombreAleatorio, 1, llave, i);
                 clientes[i].start();
